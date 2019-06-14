a952eccae92feecd1b1d09daeb00f4c27ef3b35f
Kantandia <taylorsdsmith@gmail.com>
2019-06-13 20:31:56 -0500

    js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAJGfjauMDLHfX-UrYw5hfI6qge-72xX94",
+    authDomain: "train-ec620.firebaseapp.com",
+    databaseURL: "https://train-ec620.firebaseio.com",
+    projectId: "train-ec620",
+    storageBucket: "train-ec620.appspot.com",
+    messagingSenderId: "390685970881",
+    appId: "1:390685970881:web:e1441dea655d6de1"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
